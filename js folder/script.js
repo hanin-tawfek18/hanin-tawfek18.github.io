adf15f0975adcd4102062f9d37fff28801ae2b54
hanin-tawfek18 <167953175+hanin-tawfek18@users.noreply.github.com>
2024-04-24 06:57:34 +0200

    hanin - initial version

---
+++ js folder/script.js
@@ -0,0 +1,39 @@
+function openfunction(){
+    document.getElementById('menu').style.width ='100%';
+}
+function closefunction(){
+    document.getElementById('menu').style.width = '0%'
+}
+
+function  myFunctionMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}  
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+function filterFunction() {
+  const input = document.getElementById("myInput");
+  const filter = input.value.toUpperCase();
+  const div = document.getElementById("myDropdown");
+  const a = div.getElementsByTagName("a");
+  for (let i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+  
+
---
+++ js folder/script2.js
@@ -0,0 +1,10 @@
+function openfunction(){
+    document.getElementById('menu').style.width ='100%';
+}
+function closefunction(){
+    document.getElementById('menu').style.width = '0%'
+}
+function myFunctionMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}  
